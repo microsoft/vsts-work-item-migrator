7cf09237dea52df266d2ad91f0b7871faf95f00b
Sean Ferguson <sferg@microsoft.com>
2019-04-27 12:53:01 -0700

    handle project scoped urls for inline attachments

--- UnitTests/Migration/MigrationHelpersTests.cs
+++ UnitTests/Migration/MigrationHelpersTests.cs
@@ -13,7 +13,7 @@ namespace UnitTests.Migration
         public void GetInlineImageUrlsFromField_ReturnsEmptyHashSetWhenHtmlFieldContentIsNull()
         {
             string fieldHtmlContent = null;
-            HashSet<string> actual = MigrationHelpers.GetInlineImageUrlsFromField(fieldHtmlContent, "https://dev.azure.com/account/");
+            HashSet<string> actual = MigrationHelpers.GetInlineImageUrlsFromField(fieldHtmlContent, "https://dev.azure.com/account");
             Assert.IsTrue(actual.Count == 0);
         }
 
@@ -21,7 +21,7 @@ namespace UnitTests.Migration
         public void GetInlineImageUrlsFromField_ReturnsEmptyHashSetWhenHtmlFieldContentIsEmptyString()
         {
             string fieldHtmlContent = string.Empty;
-            HashSet<string> actual = MigrationHelpers.GetInlineImageUrlsFromField(fieldHtmlContent, "https://dev.azure.com/account/");
+            HashSet<string> actual = MigrationHelpers.GetInlineImageUrlsFromField(fieldHtmlContent, "https://dev.azure.com/account");
             Assert.IsTrue(actual.Count == 0);
         }
 
@@ -29,7 +29,7 @@ namespace UnitTests.Migration
         public void GetInlineImageHtmlTags_ReturnsEmptyHashSetWhenAccountUrlDoesNotMatch()
         {
             string input = "In the workItemRelations api, we return link information, but do not tell them if it's a forward/reverse link, nor do we tell them what the opposite direction of the link is.<div><br></div><div>This is handy information to know so that consumers don't have to do string parsing based on if the reference name ends in -Forward/-Reverse to figure it out.</div><div><br></div><div><img src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\" style=\"width:779.45px;\"><br>&nbsp;<br></div>";
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account2/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account2");
             Assert.IsTrue(actual.Count == 0);
         }
 
@@ -40,7 +40,7 @@ namespace UnitTests.Migration
 
             IList<string> expected = new List<string>();
 
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
             Assert.AreEqual(expected.Count, actual.Count);
             Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
         }
@@ -53,7 +53,20 @@ namespace UnitTests.Migration
             IList<string> expected = new List<string>();
             expected.Add("<img src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\" style=\"width:779.45px;\">");
 
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
+        }
+
+        [TestMethod]
+        public void GetInlineImageHtmlTags_ReturnsCorrectValueForOneProjectScopedInlineImageHtmlTag()
+        {
+            string input = "In the workItemRelations api, we return link information, but do not tell them if it's a forward/reverse link, nor do we tell them what the opposite direction of the link is.<div><br></div><div>This is handy information to know so that consumers don't have to do string parsing based on if the reference name ends in -Forward/-Reverse to figure it out.</div><div><br></div><div><img src=\"https://dev.azure.com/account/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb/_apis/wit/attachments/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa?fileName=temp1498074488622.png\" style=\"width:779.45px;\"><br>&nbsp;<br></div>";
+
+            IList<string> expected = new List<string>();
+            expected.Add("<img src=\"https://dev.azure.com/account/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb/_apis/wit/attachments/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa?fileName=temp1498074488622.png\" style=\"width:779.45px;\">");
+
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
             Assert.AreEqual(expected.Count, actual.Count);
             Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
         }
@@ -66,7 +79,7 @@ namespace UnitTests.Migration
             IList<string> expected = new List<string>();
             expected.Add("<img style=\"width:779.45px;\" src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\">");
 
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
             Assert.AreEqual(expected.Count, actual.Count);
             Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
         }
@@ -80,7 +93,7 @@ namespace UnitTests.Migration
             expected.Add("<img src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=bbbbbbbb-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\" style=\"width:779.45px;\">");
             expected.Add("<img src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\" style=\"width:779.45px;\">");
 
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
             Assert.AreEqual(expected.Count, actual.Count);
             Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
         }
@@ -93,7 +106,7 @@ namespace UnitTests.Migration
             IList<string> expected = new List<string>();
             expected.Add("<img src=\"https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=bbbbbbbb-aaaa-aaaa-aaaa-aaaaaaaaaaaa&amp;FileName=temp1498074488622.png\" style=\"width:779.45px;\">");
 
-            IList<string> actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account/");
+            var actual = MigrationHelpers.GetInlineImageHtmlTags(input, "https://dev.azure.com/account");
             Assert.AreEqual(expected.Count, actual.Count);
             Assert.IsTrue(expected.Intersect(actual).Count() == expected.Count);
         }
@@ -117,11 +130,30 @@ namespace UnitTests.Migration
         }
 
         [TestMethod]
-        public void ReplaceAttachmentUrlGuid_ReplacesCorretly()
+        public void GetAttachmentUrlGuid_ReturnsCorrectValueForProjectScopedUrl()
+        {
+            string url = "https://dev.azure.com/account/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb/_apis/wit/attachments/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa?fileName=temp149807448ZZZZ.png";
+            Guid expected = new Guid("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+            Guid actual = MigrationHelpers.GetAttachmentUrlGuid(url);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReplaceAttachmentUrlGuid_ReplacesCorrectly()
         {
             string url = "https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&FileName=temp149807448ZZZZ.png";
             string newGuid = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb";
             string expected = "https://dev.azure.com/account/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb&FileName=temp149807448ZZZZ.png";
+            string actual = MigrationHelpers.ReplaceAttachmentUrlGuid(url, newGuid);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReplaceAttachmentUrlGuid_ReplacesCorrectlyForProjectScopedUrl()
+        {
+            string url = "https://dev.azure.com/account/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb/_apis/wit/attachments/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa?fileName=temp149807448ZZZZ.png";
+            string newGuid = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb";
+            string expected = "https://dev.azure.com/account/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb/_apis/wit/attachments/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb?fileName=temp149807448ZZZZ.png";
             string actual = MigrationHelpers.ReplaceAttachmentUrlGuid(url, newGuid);
             Assert.AreEqual(expected, actual);
         }
